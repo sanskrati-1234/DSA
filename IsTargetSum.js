99d3f4f6b22821b98febd267e2f0c378b63d8721
sanskrati-1234 <sanskratiagrawal306@gmail.com>
2024-04-29 21:43:18 +0530

    dsa

--- IsTargetSum.js
+++ IsTargetSum.js
@@ -2,22 +2,56 @@
 // Function chould return True ar false if it is bassible to generete the targetsum using cumbers numbers
 //are non-negatine and number can be used multips times consum (7, 65, 3,4,7]) → Touc
 
-function isTargetSun(arr, sum, target) {
-  if (sum === target) return true;
-  if (sum > target) {
-    return false;
-  }
-  if (arr.length < 1) {
-    return false;
-  }
-  if (
-    isTargetSun(arr.slice(1), sum, target) ||
-    isTargetSun(arr, sum + arr[0], target)
-  ) {
-    return true;
-  }
+// without memo
 
+// function isTargetSun(arr, sum, target) {
+//   if (sum === target) return true;
+//   if (sum > target) {
+//     return false;
+//   }
+//   if (arr.length < 1) {
+//     return false;
+//   }
+//   if (
+//     isTargetSun(arr.slice(1), sum, target) ||
+//     isTargetSun(arr, sum + arr[0], target)
+//   ) {
+//     return true;
+//   }
+
+//   return false;
+// }
+
+//console.log(isTargetSun([3, 3, 3], 0, 10000));
+// from top to bottom
+
+// const isTargetSum = (arr, target) => {
+//   if (target === 0) return true;
+//   if (target < 0) return false;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (isTargetSum(arr, target - arr[i])) {
+//       return true;
+//     }
+//   }
+//   return false;
+// };
+
+// console.log(isTargetSum([2, 4, 5], 10000));
+
+// memo
+
+const isTargetSum = (arr, target, memo = {}) => {
+  if (target in memo) return memo[target];
+  if (target === 0) return true;
+  if (target < 0) return false;
+
+  for (let i = 0; i < arr.length; i++) {
+    memo[target - arr[i]] = isTargetSum(arr, target - arr[i]);
+    if (memo[target - arr[i]]) {
+      return true;
+    }
+  }
   return false;
-}
-
-console.log(isTargetSun([3, 5, 5], 0, 7));
+};
+console.log(isTargetSum([2, 4, 5], 11000));
---
+++ TargetSumArray.js
@@ -0,0 +1,18 @@
+function targetSumArray(numbers, target, memo = {}) {
+  if (memo[target]) return memo[target];
+  if (target === 0) return [];
+  if (target < 0) return null;
+
+  for (let num of numbers) {
+    let rem = target - num;
+    let remArr = targetSumArray(numbers, rem);
+    if (remArr) {
+      memo[target] = [num, ...remArr];
+      return [num, ...remArr];
+    }
+  }
+  memo[target] = null;
+  return memo[target];
+}
+
+console.log("targetSumArray", targetSumArray([7], 7));
